e8cec5089590a14cdcadc293e889824b81cbcdea
Domingos Germano Franco <martinhocarneiros@gmail.com>
2026-09-12 16:24:17 +0000

    feat(qr): wire generateCadete and admitCadete controllers

--- src/interfaces/http/controllers/QrController.ts
+++ src/interfaces/http/controllers/QrController.ts
@@ -2,6 +2,8 @@ import { FastifyReply, FastifyRequest } from "fastify"
 import { ScanRouteQrUseCase } from "../../../application/qr/useCases/ScanRouteQr"
 import { GenerateBoardingQrUseCase } from "../../../application/qr/useCases/GenerateBoardingQr"
 import { ScanBoardingQrUseCase } from "../../../application/qr/useCases/ScanBoardingQr"
+import { GenerateCadeteQrUseCase } from "../../../application/qr/useCases/GenerateCadeteQr"
+import { AdmitCadeteByQrUseCase } from "../../../application/qr/useCases/AdmitCadeteByQr"
 import { ApplicationError } from "../../../application/errors/ApplicationError"
 import { JWTPayload } from "../../../utils/jwt"
 
@@ -10,6 +12,8 @@ export class QrController {
     private readonly scanRouteQr: ScanRouteQrUseCase,
     private readonly generateBoardingQr: GenerateBoardingQrUseCase,
     private readonly scanBoardingQr: ScanBoardingQrUseCase,
+    private readonly generateCadeteQr: GenerateCadeteQrUseCase,
+    private readonly admitCadeteByQr: AdmitCadeteByQrUseCase,
   ) {}
 
   async scanRoute(req: FastifyRequest<{ Body: { qr: string } }>, reply: FastifyReply) {
@@ -35,7 +39,38 @@ export class QrController {
   async scanBoarding(req: FastifyRequest<{ Body: { qr: string } }>, reply: FastifyReply) {
     try {
       const user = req.user as JWTPayload
-      const result = await this.scanBoardingQr.execute({ cadeteId: Number(user.id), role: user.role, qr: req.body?.qr })
+      const result = await this.scanBoardingQr.execute({
+        cadeteId: Number(user.id),
+        role: user.role,
+        qr: req.body?.qr,
+      })
+      return reply.send(result)
+    } catch (error) {
+      return this.handle(error, reply)
+    }
+  }
+
+  async generateCadete(req: FastifyRequest, reply: FastifyReply) {
+    try {
+      const user = req.user as JWTPayload
+      const result = await this.generateCadeteQr.execute({
+        cadeteId: Number(user.id),
+        role: user.role,
+      })
+      return reply.send(result)
+    } catch (error) {
+      return this.handle(error, reply)
+    }
+  }
+
+  async admitCadete(req: FastifyRequest<{ Body: { qr: string } }>, reply: FastifyReply) {
+    try {
+      const user = req.user as JWTPayload
+      const result = await this.admitCadeteByQr.execute({
+        driverId: Number(user.id),
+        role: user.role,
+        qr: req.body?.qr,
+      })
       return reply.send(result)
     } catch (error) {
       return this.handle(error, reply)
